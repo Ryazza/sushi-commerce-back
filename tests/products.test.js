9fc5ff6a93787a3088aef3d2df2f39a8c39ac4e4
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-07-23 13:26:06 +0200

    test get ok

--- tests/products.test.js
+++ tests/products.test.js
@@ -4,25 +4,34 @@ const request = require("supertest");
 const express = require("express");
 const app = express();
 
+const createForm = {
+    name: "intello",
+    category: "processeurs",
+    description: "il est bien il est beau, il sent bon le sable chaud.",
+    pictures: ["jolie_image_de_proc"],
+    events: ["blah"],
+    stock:["12"] ,
+    price: 22
+}
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use("/", index);
 
-test("index route works", done => {
+test("get all route works", done => {
     request(app)
         .get("/product/")
         .expect("Content-Type", /json/)
+        // .expect({ name })
         .expect(200, done);
 });
 
-test("testing route works", done => {
+test("create route works", done => {
     request(app)
-        .post("/test")
+        .post("/create")
         .type("form")
-        .send({ item: "hey" })
-        .then(() => {
-            request(app)
-                .get("/test")
-                .expect({ array: ["hey"] }, done);
+        .send(createForm)
+        .then((data) => {
+           data.expect({ success: true }, done);
         });
 });
