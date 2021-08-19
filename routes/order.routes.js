d6da0d962438fb9a2e094a48b2c7e824f0bcf558
Emeline <marie.le-du@epitech.eu>
2021-08-19 11:38:32 +0200

    new route order id admin

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -20,5 +20,6 @@ router.delete('/:id', checkTokenMiddleware.checkToken, orderController.deleteOrd
 router.get('/', checkTokenMiddleware.checkTokenAdmin, orderController.getAllOrder)
 router.get('/status/:status/:order', checkTokenMiddleware.checkTokenAdmin, orderController.getAllOrderByStatus)
 router.put('/status/newStatus/:id', checkTokenMiddleware.checkTokenAdmin, orderController.updateStatus)
+router.get('/admin/order/:id', checkTokenMiddleware.checkTokenAdmin, orderController.getOneOrder)
 
 module.exports = router;
